b7e9b9eac2d132802b3cd0eb45f37025dc6fd6ea
omorsultan <omorsultansust@gmail.com>
2026-06-30 15:28:05 +0600

    stritng

---
+++ array.js
@@ -0,0 +1,11 @@
+arr =  [1, 6, 8, 3] ;
+
+
+
+console.log(arr);
+
+brr = [1,2,5 ,23, 55,67 ]
+
+brr.forEach(element => {
+  console.log(element);
+});
---
+++ strings.js
@@ -0,0 +1,14 @@
+let s = "omor sultan";
+
+console.log(s , s.length);
+
+
+for( let i= 0 ;i < s.length ; i++){
+  console.log(s[i]);
+}
+
+
+for( const char of s ) {
+  console.log(char);
+}
+
