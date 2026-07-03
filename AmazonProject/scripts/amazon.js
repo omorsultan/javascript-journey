6fb898ab058de13687e291839ff5d4bde15deb06
omorsultan <omorsultansust@gmail.com>
2026-07-03 11:24:42 +0600

    change the dummy value by object . print in console

--- AmazonProject/scripts/amazon.js
+++ AmazonProject/scripts/amazon.js
@@ -44,6 +44,56 @@ const products = [{
 
 products.forEach((product)=>{ // inside create a function , pass parameter product. 
 // check korbo product er html code kothai ache. otake hover kore then html code e class ta search korbo . poro code ta copy korbo
-const html = ``; // ekhane paste korbo
+const html = `<div class="product-container">
+                <div class="product-image-container">
+                  <img class="product-image"
+                    src="${product.image}">
+                </div>
+
+                <div class="product-name limit-text-to-2-lines">
+                ${product.name }
+                </div>
+
+                <div class="product-rating-container">
+                  <img class="product-rating-stars"
+                    src="images/ratings/rating-${product.rating.stars *10 }.png">
+                  <div class="product-rating-count link-primary">
+                    ${product.rating.count}
+                  </div>
+                </div>
+
+                <div class="product-price">
+                  $${product.priceCents / 100}
+                </div>
+
+                <div class="product-quantity-container">
+                  <select>
+                    <option selected value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                  </select>
+                </div>
+
+                <div class="product-spacer"></div>
+
+                <div class="added-to-cart">
+                  <img src="images/icons/checkmark.png">
+                  Added
+                </div>
+
+                <button class="add-to-cart-button button-primary">
+                  Add to Cart
+                </button>
+              </div>`; // ekhane paste korbo
+              // tab press kore indexing thik rakhbo
+    console.log(html); // kar kore kina check ?
 
 }); // loop through this array using a for each method
+// in this code there are fixed value but we want to change this
