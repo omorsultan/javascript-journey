a179adcecd4137a31110dc1f6480ac9e0a126c5f
omorsultan <omorsultansust@gmail.com>
2026-07-04 13:04:11 +0600

    format currency needs  in different two  file . now combine  it buymodule , function and utils

--- AmazonProject/scripts/checkout.js
+++ AmazonProject/scripts/checkout.js
@@ -1,5 +1,6 @@
 import{cart} from '../data/cart.js';
 import{products} from '../data/products.js';
+import { formatCurrency } from './utils/money.js';
 
 let cartSummeryHTML = '';
  cart.forEach((cartItem)=>{
@@ -30,7 +31,7 @@ let cartSummeryHTML = '';
                   ${matchingProduct.name}
                 </div>
                 <div class="product-price">
-                  $${(matchingProduct.priceCents/100).toFixed(2)}
+                  $${formatCurrency(matchingProduct.priceCents)}
                 </div>
                 <div class="product-quantity">
                   <span>
