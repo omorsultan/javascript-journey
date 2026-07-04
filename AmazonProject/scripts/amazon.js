ecfb3bb682d3a17f5bb7def2131be448ca3e2b7b
omorsultan <omorsultansust@gmail.com>
2026-07-04 07:17:27 +0600

    update quantity only

--- AmazonProject/scripts/amazon.js
+++ AmazonProject/scripts/amazon.js
@@ -106,14 +106,26 @@ document.querySelectorAll('.js-add-to-cart')
   .forEach((button)=>{ 
     button.addEventListener('click',()=>{
      const productName = button.dataset.productName;
+//------ update quantity only-------//
+     let matchingItem;
 
-     cart.push ({
-      productName: productName,
-      quantity : 1
-     });
-     console.log(cart);
-    });
+     cart.forEach((item)=>{
+      if(productName === item.productName){
+        matchingItem = item;
+      }
+     })
 
+     if(matchingItem){
+      matchingItem.quantity +=1;
+     }
+     else{
+       cart.push ({
+        productName: productName,
+        quantity : 1
+      });
+    }
+      console.log(cart);
   });
+});
 
  
